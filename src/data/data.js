dc40788485fcaccdb62ef1993737520f26516d1d
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-12-10 11:14:38 -0500

    updates

--- src/data/data.js
+++ src/data/data.js
@@ -2,12 +2,65 @@ const data = {
   posts: [{
     imageRef: "/parentconnect.png",
     type: "Post",
-    title: "Placholder",
-    date: "12/31/20",
-    description: "This is placeholder, because I have yet to write a post",
+    title: "3rd Times the Charm (Portfolio Design and Devs)",
+    date: "12/4/20",
+    description: "My design and development journey from developing three seperate portfolio applications.",
+    body: ["There is no body yet"],
     color: 'parentConnect'
   }],
   projects:[
+    {
+      type: "Project",
+      date: "11/15/20",
+      title: "Password Generator 1.1 MDC",
+      description: "Stay safe with secure passwords.",
+      imageRef: "/password2.png",
+      repoLink: "github.com/mitchellmunderwood/Password-Generator-1.1-MDC",
+      liveLink: "mitchellmunderwood.github.io/Password-Generator-1.1-MDC/",
+      badges: [
+        {type: "Code", name: "JS"},
+        {type: "Code", name: "HTML"},
+        {type: "Code", name: "CSS"},
+        {type: "Code", name: "Material Design Components"},
+      ],
+      color: "password2"
+    },
+    {
+      type: "Project",
+      date: "11/25/20",
+      title: "React Portfolio 1.2",
+      description: "A minimalist portfolio, built in React.",
+      imageRef: "/portfolio2.png",
+      repoLink: "github.com/mitchellmunderwood/Portfolio-1.2-React",
+      liveLink: "mitchellmunderwood.github.io/Portfolio-1.2-React/",
+      badges: [
+        {type: "Code", name: "JS"},
+        {type: "Code", name: "HTML"},
+        {type: "Code", name: "CSS"},
+        {type: "Code", name: "Material Design Components"},
+      ],
+      color: "portfolio2"
+    },
+    {
+      type: "Project",
+      date: "10/20/20",
+      title: "PolitiCandid",
+      description: "Know where people stand on the important issues.",
+      imageRef: "/politicandid.png",
+      repoLink: "github.com/mitchellmunderwood/PolitiCandid",
+      liveLink: "mu-politicandid.herokuapp.com/",
+      badges: [
+        {type: "Code", name: "JS"},
+        {type: "Code", name: "HTML"},
+        {type: "Code", name: "CSS"},
+        {type: "Code", name: "React"},
+        {type: "Code", name: "Material UI"},
+        {type: "Code", name: "Express"},
+        {type: "Code", name: "NodeJS"},
+        {type: "Data", name: "MongoDB"},
+      ],
+      color: "politiCandid"
+    },
     {
       type: "Project",
       date: "8/29/20",
